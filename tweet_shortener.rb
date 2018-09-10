1b679d8f80451d66619831af3e0ae3826d8be328
Steven Zapata <szapata427@gmail.com>
2018-09-10 15:31:23 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,17 @@
 def dictionary
-  
+    words_sub = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+    }
+end
 
 def word_substituter(tweet_one)
   
