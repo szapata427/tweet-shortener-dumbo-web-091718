3f3285a7c7fad4e5d0cf7d79fd8caac766f40d7a
Steven Zapata <szapata427@gmail.com>
2018-09-10 17:29:56 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,13 +38,13 @@ def selective_tweet_shortener(tweet)
   end
   
   def shortened_tweet_truncator(tweet)
-    realtweet = word_substituter(tweet)
-    if realtweet.length > 140
-      realtweet(0...140)
-    else 
-      tweet
-    end
+    short = word_substituter(tweet)
+  if short.length > 140
+    short[0...140]
+  else
+    short
   end
+end
   
       
       
